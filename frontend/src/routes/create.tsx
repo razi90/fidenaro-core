8ada0e479c8a9a010f88db4c96223fecfa992a3d
Razi Corleone <andreas.razmyslov@gmail.com>
2023-03-19 18:57:43 +0100

    Improved style

--- frontend/src/routes/create.tsx
+++ frontend/src/routes/create.tsx
@@ -1,4 +1,4 @@
-import { SetStateAction, useState } from "react";
+import { ReactNode, SetStateAction, useState } from "react";
 import {
     Box,
     Heading,
@@ -12,19 +12,35 @@ import {
     Image,
     Button,
     Stack,
-    Text
+    Text,
+    useColorModeValue,
+    HStack,
 } from "@chakra-ui/react";
 
 const assets = [
-    { name: "Bitcoin", image: "https://via.placeholder.com/50x50" },
-    { name: "Ethereum", image: "https://via.placeholder.com/50x50" },
-    { name: "Radix", image: "https://via.placeholder.com/50x50" },
+    { name: "Bitcoin", image: "https://altcoinsbox.com/wp-content/uploads/2022/12/bitcoin-logo-300x300.webp" },
+    { name: "Ethereum", image: "https://altcoinsbox.com/wp-content/uploads/2023/01/ethereum-logo-300x300.webp" },
+    { name: "Radix", image: "https://pbs.twimg.com/profile_images/1517539895761330177/yNSrmKk5_400x400.jpg" },
 ];
+
+function VaultWrapper({ children }: { children: ReactNode }) {
+    return (
+        <Box
+            mb={4}
+            shadow="base"
+            borderWidth="1px"
+            alignSelf={{ base: 'center', lg: 'flex-start' }}
+            borderColor={useColorModeValue('gray.200', 'gray.500')}
+            borderRadius={'xl'}>
+            {children}
+        </Box>
+    );
+}
 
 export default function Create() {
     const [vaultName, setVaultName] = useState("");
     const [selectedAsset, setSelectedAsset] = useState<{ name: string; image: string; } | null>(null);
-    const [performanceFee, setPerformanceFee] = useState(10);
+    const [performanceFee, setPerformanceFee] = useState(30);
 
     const handleVaultNameChange = (event: { target: { value: SetStateAction<string>; }; }) => {
         console.log(event.target.value)
@@ -63,59 +79,71 @@ export default function Create() {
                 </Stack>
             </Box>
             <form onSubmit={handleSubmit}>
-                <FormControl mb={4}>
-                    <FormLabel>Choose a name for your vault</FormLabel>
-                    <Input type="text" value={vaultName} onChange={handleVaultNameChange} />
-                </FormControl>
-                <FormControl mb={4}>
-                    <FormLabel>Choose the asset to trade</FormLabel>
-                    {assets.map((asset) => (
-                        <Box
-                            key={asset.name}
-                            as="label"
-                            display="flex"
-                            alignItems="center"
-                            mb={2}
-                            cursor="pointer"
-                        >
-                            <input
-                                type="radio"
-                                name="asset"
-                                value={asset.name}
-                                checked={selectedAsset === asset}
-                                onChange={() => handleAssetSelection(asset)}
-                                style={{ display: "none" }}
-                            />
-                            <Box
-                                borderWidth={1}
-                                borderRadius="md"
-                                borderColor="gray.200"
-                                p={2}
-                                mr={2}
+                <Stack
+                    justify="center"
+                    spacing={{ base: 4, lg: 10 }}
+                    py={10}>
+                    <VaultWrapper>
+
+                        <FormControl mb={4}>
+                            <FormLabel>Choose a name for your vault</FormLabel>
+                            <Input type="text" value={vaultName} onChange={handleVaultNameChange} />
+                        </FormControl>
+                        <FormControl mb={4}>
+                            <FormLabel>Choose the asset to trade</FormLabel>
+                            <HStack>
+                                {assets.map((asset) => (
+                                    <Box
+                                        key={asset.name}
+                                        as="label"
+                                        display="flex"
+                                        alignItems="center"
+                                        cursor="pointer"
+                                        bg={selectedAsset === asset ? "blue.100" : "transparent"} // add conditional styling here
+                                        borderRadius="md"
+                                        boxShadow={selectedAsset === asset ? "md" : "none"} // add conditional styling here
+                                        transition="all 0.2s"
+                                        _hover={{ boxShadow: "md" }}
+                                    >
+                                        <input
+                                            type="radio"
+                                            name="asset"
+                                            value={asset.name}
+                                            checked={selectedAsset === asset}
+                                            onChange={() => handleAssetSelection(asset)}
+                                            style={{ display: "none" }}
+                                        />
+                                        <Box p={2}>
+                                            <Image
+                                                src={asset.image}
+                                                alt={asset.name}
+                                                boxSize="50px"
+                                                borderRadius="full" // add borderRadius property here
+                                            />
+                                        </Box>
+                                    </Box>
+                                ))}
+                            </HStack>
+                        </FormControl>
+                        <FormControl mb={4}>
+                            <FormLabel>Performance fee ({performanceFee}%)</FormLabel>
+                            <Slider
+                                defaultValue={performanceFee}
+                                min={10}
+                                max={50}
+                                step={1}
+                                onChange={handlePerformanceFeeChange}
                             >
-                                <Image src={asset.image} alt={asset.name} boxSize="50px" />
-                            </Box>
-                            <span>{asset.name}</span>
-                        </Box>
-                    ))}
-                </FormControl>
-                <FormControl mb={4}>
-                    <FormLabel>Performance fee ({performanceFee}%)</FormLabel>
-                    <Slider
-                        defaultValue={performanceFee}
-                        min={10}
-                        max={50}
-                        step={1}
-                        onChange={handlePerformanceFeeChange}
-                    >
-                        <SliderTrack>
-                            <SliderFilledTrack />
-                        </SliderTrack>
-                        <SliderThumb />
-                    </Slider>
-                </FormControl>
-                <Button type="submit" colorScheme="blue"></Button>
+                                <SliderTrack>
+                                    <SliderFilledTrack />
+                                </SliderTrack>
+                                <SliderThumb />
+                            </Slider>
+                        </FormControl>
+                        <Button type="submit" colorScheme="blue">Create</Button>
+                    </VaultWrapper>
+                </Stack>
             </form>
-        </Box>
+        </Box >
     );
 }
