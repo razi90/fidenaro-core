8dd631b4b124c17fb0b21d99f75fb1604cd57ce1
Razi Corleone <andreas.razmyslov@gmail.com>
2023-03-21 23:57:08 +0100

    Creating vaults works

--- frontend/src/routes/create_vault/create-vault-api.js
+++ frontend/src/routes/create_vault/create-vault-api.js
@@ -6,7 +6,7 @@ import {
     ResourceAddress
 } from '@radixdlt/radix-dapp-toolkit'
 import { TransactionApi } from "@radixdlt/babylon-gateway-api-sdk";
-import { dAppPackageAddress, dAppId, dAppName } from '../../etc/globals.d';
+import { dAppPackageAddress, dAppId, dAppName, radswapPoolComponentAddress } from '../../etc/globals.d';
 
 // Instantiate Gateway SDK
 const transactionApi = new TransactionApi();
@@ -15,9 +15,6 @@ const rdt = RadixDappToolkit(
     (requestData) => {
         requestData({
             accounts: { quantifier: 'atLeast', quantity: 1 },
-        }).map(({ data: { accounts } }) => {
-            // add accounts to dApp application state
-            console.log("account data: ", accounts)
         })
     },
     { networkId: 11 }
@@ -25,12 +22,14 @@ const rdt = RadixDappToolkit(
 console.log("dApp Toolkit: ", rdt)
 
 const accountAddress = localStorage.getItem('account')
+let resourceAddress
+let componentAddress
 
 export async function createVauĺt() {
 
     let manifest = new ManifestBuilder()
         .callMethod(accountAddress, "create_proof", [ResourceAddress("resource_tdx_b_1qqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqq8z96qp")])
-        .callFunction(dAppPackageAddress, "TradeVault", "init_trade_vault", [ComponentAddress(accountAddress), Decimal("10"), ComponentAddress(dAppId)])
+        .callFunction(dAppPackageAddress, "TradeVault", "init_trade_vault", [ComponentAddress(accountAddress), Decimal("10"), ComponentAddress(radswapPoolComponentAddress)])
         .build()
         .toString();
 
@@ -55,22 +54,21 @@ export async function createVauĺt() {
     });
     console.log('Instantiate TransactionApi transaction/status:', status)
 
-    // // ************* fetch component address from gateway api and set componentAddress variable **************
-    // let commitReceipt = await transactionApi.transactionCommittedDetails({
-    //     transactionCommittedDetailsRequest: {
-    //         transaction_identifier: {
-    //             type: 'intent_hash',
-    //             value_hex: result.value.transactionIntentHash
-    //         }
-    //     }
-    // })
-    // console.log('Instantiate Committed Details Receipt', commitReceipt)
+    // ************* fetch component address from gateway api and set componentAddress variable **************
+    let commitReceipt = await transactionApi.transactionCommittedDetails({
+        transactionCommittedDetailsRequest: {
+            transaction_identifier: {
+                type: 'intent_hash',
+                value_hex: result.value.transactionIntentHash
+            }
+        }
+    })
+    console.log('Instantiate Committed Details Receipt', commitReceipt)
 
     // ****** set componentAddress and resourceAddress variables with gateway api commitReciept payload ******
-    // componentAddress = commitReceipt.details.receipt.state_updates.new_global_entities[0].global_address <- long way -- shorter way below ->
-    // componentAddress = commitReceipt.details.referenced_global_entities[0]
-    // document.getElementById('componentAddress').innerText = componentAddress;
+    componentAddress = commitReceipt.details.referenced_global_entities[0]
+    console.log(componentAddress)
 
-    // resourceAddress = commitReceipt.details.referenced_global_entities[1]
-    // document.getElementById('gumAddress').innerText = resourceAddress;
+    resourceAddress = commitReceipt.details.referenced_global_entities[1]
+    console.log(resourceAddress)
 }
