629c057deb18650056b5ae348207079d05a499b9
Razi Corleone <andreas.razmyslov@gmail.com>
2023-03-21 23:32:54 +0100

    Initialized liquidity pool for BUSD and BTC

--- frontend/src/etc/globals.d.ts
+++ frontend/src/etc/globals.d.ts
@@ -1,3 +1,6 @@
 export const dAppPackageAddress: string = "package_tdx_b_1qxmzvdug22x5wza6u225y9vetq45ggjxmha8selv6d6qqzmkar";
 export const dAppId: string = "account_tdx_b_1pqhy68vjzkdkujlajld6yhspztzkh5vtxu9mvm6rw9sqdq6tcd";
 export const dAppName: string = "Fidenaro";
+
+export const radswapPackageAddress: string = "package_tdx_b_1qxty2xnw93djt5n3pt9m6dn8n06cclhs9fhtq00k5c6sma3ev2";
+export const radswapPoolComponentAddress: string = "component_tdx_b_1qg2ajt7avhpcw78rk3f4ddmg62kg5at8atq3y80k257q50shvn";
--- frontend/src/routes/create_vault/create-vault-api.js
+++ frontend/src/routes/create_vault/create-vault-api.js
@@ -2,7 +2,8 @@ import {
     RadixDappToolkit,
     ManifestBuilder,
     Decimal,
-    ComponentAddress
+    ComponentAddress,
+    ResourceAddress
 } from '@radixdlt/radix-dapp-toolkit'
 import { TransactionApi } from "@radixdlt/babylon-gateway-api-sdk";
 import { dAppPackageAddress, dAppId, dAppName } from '../../etc/globals.d';
@@ -11,6 +12,14 @@ import { dAppPackageAddress, dAppId, dAppName } from '../../etc/globals.d';
 const transactionApi = new TransactionApi();
 const rdt = RadixDappToolkit(
     { dAppDefinitionAddress: dAppId, dAppName: dAppName },
+    (requestData) => {
+        requestData({
+            accounts: { quantifier: 'atLeast', quantity: 1 },
+        }).map(({ data: { accounts } }) => {
+            // add accounts to dApp application state
+            console.log("account data: ", accounts)
+        })
+    },
     { networkId: 11 }
 )
 console.log("dApp Toolkit: ", rdt)
@@ -20,10 +29,13 @@ const accountAddress = localStorage.getItem('account')
 export async function createVauĺt() {
 
     let manifest = new ManifestBuilder()
+        .callMethod(accountAddress, "create_proof", [ResourceAddress("resource_tdx_b_1qqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqq8z96qp")])
         .callFunction(dAppPackageAddress, "TradeVault", "init_trade_vault", [ComponentAddress(accountAddress), Decimal("10"), ComponentAddress(dAppId)])
         .build()
         .toString();
+
     console.log("Instantiate Manifest: ", manifest)
+
     // Send manifest to extension for signing
     const result = await rdt
         .sendTransaction({
@@ -43,16 +55,16 @@ export async function createVauĺt() {
     });
     console.log('Instantiate TransactionApi transaction/status:', status)
 
-    // ************* fetch component address from gateway api and set componentAddress variable **************
-    let commitReceipt = await transactionApi.transactionCommittedDetails({
-        transactionCommittedDetailsRequest: {
-            transaction_identifier: {
-                type: 'intent_hash',
-                value_hex: result.value.transactionIntentHash
-            }
-        }
-    })
-    console.log('Instantiate Committed Details Receipt', commitReceipt)
+    // // ************* fetch component address from gateway api and set componentAddress variable **************
+    // let commitReceipt = await transactionApi.transactionCommittedDetails({
+    //     transactionCommittedDetailsRequest: {
+    //         transaction_identifier: {
+    //             type: 'intent_hash',
+    //             value_hex: result.value.transactionIntentHash
+    //         }
+    //     }
+    // })
+    // console.log('Instantiate Committed Details Receipt', commitReceipt)
 
     // ****** set componentAddress and resourceAddress variables with gateway api commitReciept payload ******
     // componentAddress = commitReceipt.details.receipt.state_updates.new_global_entities[0].global_address <- long way -- shorter way below ->
