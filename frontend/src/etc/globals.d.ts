f445f40056ae36b43e4f35e684c6a4d73cd0e171
Razi Corleone <andreas.razmyslov@gmail.com>
2023-04-17 21:26:37 +0200

    Deployed new packages and updated addresses Added wallet sdk to yarn

--- frontend/src/etc/globals.d.ts
+++ frontend/src/etc/globals.d.ts
@@ -1,6 +1,8 @@
-export const dAppPackageAddress: string = "package_tdx_b_1qxmzvdug22x5wza6u225y9vetq45ggjxmha8selv6d6qqzmkar";
-export const dAppId: string = "account_tdx_b_1pqhy68vjzkdkujlajld6yhspztzkh5vtxu9mvm6rw9sqdq6tcd";
+export const dAppPackageAddress: string = "package_tdx_c_1qqd6cm4xq0r7p7u7x67kgtj0q0a5vk7w7j2ce5xt8c9qvjzxny";
+export const dAppId: string = "account_tdx_c_1p8yfhagyxxd43r53h8uznduqr2tthtkgrn7tr47q6dgssvp2dk";
 export const dAppName: string = "Fidenaro";
 
-export const radswapPackageAddress: string = "package_tdx_b_1qxty2xnw93djt5n3pt9m6dn8n06cclhs9fhtq00k5c6sma3ev2";
+export const radswapPackageAddress: string = "package_tdx_c_1qpccz7knjpqlz7tlmtglen7mr7n7gukt0qpu9nt4ad7sam9x5r";
+
+//TODO: Create new pool
 export const radswapPoolComponentAddress: string = "component_tdx_b_1qg2ajt7avhpcw78rk3f4ddmg62kg5at8atq3y80k257q50shvn";
