6bab5ac6fad3c019a69b50dd2647f76697f63e48
Jan Meurer <jan.meurer@gmx.de>
2023-12-25 16:26:59 +0000

    Disable mobile version sizes with hint.

--- ui/src/Layout.tsx
+++ ui/src/Layout.tsx
@@ -13,9 +13,13 @@ import TopNavigationBar from './components/TopNavigationBar/TopNavigationBar';
 import {
     Text,
     Flex,
+    Box,
+    Center,
 } from "@chakra-ui/react";
 import { initRadixDappToolkit } from './libs/radix-dapp-toolkit/rdt';
 import { useQueryClient } from '@tanstack/react-query';
+import { routePageBoxStyle } from './libs/styles/RoutePageBox';
+import { FidenaroCircularProgress } from './components/Loading/FidenaroCircularProgress/FidenaroCircularProgress';
 
 
 
@@ -35,22 +39,48 @@ const Layout: React.FC = () => {
 
 
     return (
-        <BrowserRouter>
-            <TopNavigationBar />
-            <Flex>
-                <LeftNavigationBar isMinimized={isMinimized} setIsMinimized={setIsMinimized} />
-                <Routes>
-                    <Route path="/" element={<Explore isMinimized={isMinimized} />} />
-                    <Route path="/portfolio" element={<Portfolio isMinimized={isMinimized} />} />
-                    <Route path="/walloffame" element={<WallOfFame isMinimized={isMinimized} />} />
-                    <Route path="/profile" element={<Profile isMinimized={isMinimized} />} />
-                    <Route path="/vault/:id" element={<Vault isMinimized={isMinimized} />} />
-                    <Route
-                        path="*"
-                        element={<UnknownPage isMinimized={isMinimized} />}
-                    />
-                </Routes>
-            </Flex>
+        <BrowserRouter >
+            { /* Desktop version of the fidenaro app */}
+            <Box display={{ base: "none", sm: "none", md: "none", lg: "block" }}>
+                <TopNavigationBar />
+                <Flex >
+                    <LeftNavigationBar isMinimized={isMinimized} setIsMinimized={setIsMinimized} />
+                    <Routes>
+                        <Route path="/" element={<Explore isMinimized={isMinimized} />} />
+                        <Route path="/portfolio" element={<Portfolio isMinimized={isMinimized} />} />
+                        <Route path="/walloffame" element={<WallOfFame isMinimized={isMinimized} />} />
+                        <Route path="/profile" element={<Profile isMinimized={isMinimized} />} />
+                        <Route path="/vault/:id" element={<Vault isMinimized={isMinimized} />} />
+                        <Route
+                            path="*"
+                            element={<UnknownPage isMinimized={isMinimized} />}
+                        />
+                    </Routes>
+                </Flex>
+            </Box>
+            { /* Placeholder for the mobile version */}
+            <Box display={{ base: "block", sm: "block", md: "block", lg: "none" }}>
+                <Box
+                    bg={"back.900"}
+                    w="100%"
+                    h={"100vh"}
+                    alignItems="center"
+                    justifyContent="center"
+                >
+                    <Center>
+
+                        <FidenaroCircularProgress circleSize="30vh" circleBorderThickness="2px" circleImageSize="20vh" />
+
+                    </Center>
+                    <Text w="100%"
+                        align={"center"}
+                        alignItems="center"
+                        justifyContent="center" color={"grey"} fontSize={"14px"}>
+                        There is currently no support for a Mobile Version of Fidenaro. <br />
+                        Please choose a Desktop environment in order to Test the Beta.
+                    </Text>
+                </Box >
+            </Box>
         </BrowserRouter >
     )
 }
