92df7f3e6eab2df0a5fa943d18a13da66923cb43
RaziCorleone <andreas.razmyslov@gmail.com>
2024-07-17 18:00:47 +0200

    Added simple test

---
+++ scrypto/tests/tests/lib.rs
@@ -0,0 +1,24 @@
+use scrypto::math::Decimal;
+use scrypto_test::prelude::*;
+
+use radiswap::test_bindings::*;
+
+#[test]
+fn simple_radiswap_test() -> Result<(), RuntimeError> {
+    // Arrange
+    let mut ledger = LedgerSimulatorBuilder::new().build();
+    let package_address = ledger.compile_and_publish("../bootstrapping-helper/radiswap");
+    let (public_key, _private_key, account) = ledger.new_allocated_account();
+
+    let resource_address1 = ledger.create_fungible_resource(Decimal::from(100), 18, account);
+    let resource_address2 = ledger.create_fungible_resource(Decimal::from(100), 18, account);
+
+    // let mut radiswap = Radiswap::new(
+    //     OwnerRole::None,
+    //     resource_address1,
+    //     resource_address2,
+    //     package_address,
+    // )?;
+
+    Ok(())
+}
