c1080f45b27738aab5a6124b4b3db402b9fe1fc1
Razy Corleone <andreas.razmyslov@gmail.com>
2024-08-28 15:46:42 +0200

    Updated blacklist

--- ui/src/libs/fidenaro/Config.tsx
+++ ui/src/libs/fidenaro/Config.tsx
@@ -14,5 +14,13 @@ export const BLACKLIST = ["component_tdx_2_1cru00swt2hnz285jrw64cd6c5zfae28a6540
     "component_tdx_2_1cr3xe066yvlhhsn9jx7aw8z04n8rm7k459f3z42nssapz4x429n3j8",
     "component_tdx_2_1czzf7gplqclewmff65gqytem3we0pd5ma942686z8jqhkrjl3vj2ns",
     "component_tdx_2_1cpn2lxhcng8s7y5cfqk2g3fcpq5z6ncxav0nqqfcrn2emmu7f6nl57",
-    "component_tdx_2_1cqfjkhlczpm8qhw9daap7f97dwskg06zdkwe3jxu2clmdglm7as0p6"
+    "component_tdx_2_1cqfjkhlczpm8qhw9daap7f97dwskg06zdkwe3jxu2clmdglm7as0p6",
+    "component_tdx_2_1cpc0ln6e25emtt2mtv7n70msx9mgpkef06l06g4x9g8560zhlgmf2w",
+    "component_tdx_2_1czjv05qds4vkyk03m8kymvzuqf7jwd8qq6qa872m7p5s97vmdphr7w",
+    "component_tdx_2_1crl3h42whfjxyh4m0aay5y0s9dy7q4mvgc6mnh38ll8t5htwcdjnmd",
+    "component_tdx_2_1cpwwja583utmpea3yarm83grt4feyyesqhkzemzpp70467g7u57mct",
+    "component_tdx_2_1cqlp9cj78ee8ushcglekz67mvwl97pyxnkhqn7v0mtc0q0nw630xuh",
+    "component_tdx_2_1cru3xe9r045u3t9exf956fzl568cpesjlug0g7d3ytj9h945n06jr8",
+    "component_tdx_2_1cz38fng0tah2yatty4n86we9dj9sptzal8pxsyv5y8waeak9xlwvhz",
+    "component_tdx_2_1cq54nyktkse468lkn7lj2ltuse40ggeq89ap4h7kqka0cymnyjtawp",
 ]
